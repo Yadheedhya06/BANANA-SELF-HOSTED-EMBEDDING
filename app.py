39806781d373f5c198b576edf8e3f741e43747c0
Yadheedhya06 <Yadheedhya6@gmail.com>
2023-02-28 15:20:29 +0530

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain.embeddings import SelfHostedEmbeddings
+import runhouse as rh
 import torch
+
+
+gpu = rh.cluster(name="rh-a10x", instance_type="A100:1", use_spot=False)
+
+# text = input("Enter text:")
+
+def get_pipeline():
+    model_id = "bert-base-uncased"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+    return pipeline("Fill-Mask", model=model, tokenizer=tokenizer)
+
+def inference_fn(pipeline, prompt):
+    # Return last hidden state of the model
+     if isinstance(prompt, list):
+        return [emb[0][-1] for emb in pipeline(prompt)] 
+     return pipeline(prompt)[0][-1]
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -8,6 +27,7 @@ def init():
     
     device = 0 if torch.cuda.is_available() else -1
     model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -20,7 +40,17 @@ def inference(model_inputs:dict) -> dict:
         return {'message': "No prompt provided"}
     
     # Run the model
-    result = model(prompt)
+    embeddings = SelfHostedEmbeddings(
+    model_load_fn=get_pipeline, 
+    hardware=gpu,
+    model_reqs=["./", "torch", "transformers"],
+    inference_fn=inference_fn)
 
+    result = embeddings.embed_query(prompt)
     # Return the results as a dictionary
+    # result = {'output': result}
     return result
+
+# if __name__ == "__main__":
+#     init()
+#     print(inference({'prompt': text}))
